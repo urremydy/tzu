0d47a1082b423fb84e4e46de4f947c7c3adeef76
urremydy <152601521+urremydy@users.noreply.github.com>
2024-12-10 11:52:11 +0800

    Delete website directory

--- website/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
